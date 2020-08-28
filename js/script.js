20c71048388c2d0b6069baad562c0d8040f15f6b
kostya <matvienkoigor956@gmail.com>
2020-08-28 11:24:57 +0300

    changed icons

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ $(window).scroll(() => {
         header.removeClass("sticky");
         $('#coursel').css('margin-top', 0);
     }
+});
 
-
+$(".nav_link").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 500);
 });
