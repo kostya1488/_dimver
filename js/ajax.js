7ba92772bfcf8616a004647a1c1308b63a99af20
kostya <matvienkoigor956@gmail.com>
2020-08-30 14:52:15 +0300

    form handler, ajax, added content, changed icons

---
+++ js/ajax.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#btn").click(
+        function() {
+            sendAjaxForm('ajax_form', 'php/formhandler.php');
+            return false;
+        }
+    );
+});
+
+function sendAjaxForm(ajax_form, url) {
+    $.ajax({
+        url: url, //url страницы (action_ajax_form.php)
+        type: "POST", //метод отправки
+        dataType: "html", //формат данных
+        data: $("#" + ajax_form).serialize(), // Сеарилизуем объект
+        success: function(response) { //Данные отправлены успешно
+            result = $.parseJSON(response);
+            console.log(result);
+
+        },
+        error: function(response) { // Данные не отправлены
+            console.log(response);
+            console.log('Ошибка. Данные не отправлены.');
+        }
+    });
+}
---
+++ php/formhandler.php
@@ -0,0 +1,31 @@
+<?php
+$to = "matvienkoigor956@gmail.com";
+$subject = "Заявка с сайта";
+$headers = "From: message@dimver.net.ua";
+
+
+
+
+if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"]) ) { 
+
+	// Формируем массив для JSON ответа
+    $result = array(
+    	'Имя' => $_POST["name"],
+        'Email' => $_POST["email"],
+    	'Сообщение' => $_POST["message"]        
+    ); 
+
+    // Переводим массив в JSON
+    echo json_encode($result); 
+
+    foreach ($result as $key => $value) {
+        $txt_for_mail .= $key . ": " . $value ."\n";
+ }
+
+  mail($to, $subject, $txt_for_mail, $headers);
+
+}
+
+
+
+?>
